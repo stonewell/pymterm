a482381ce489282c5110690db8a6b8b70ba7a54d
Jingnan Si <jingnan.si@gmail.com>
2016-05-23 19:19:06 -0700

    using new winpty

--- pymterm/client/pipe_client.py
+++ pymterm/client/pipe_client.py
@@ -18,7 +18,9 @@ def start_client(session, cfg):
     try:
         #cmd = [r'pythonw.exe', r'c:\local\winpty\run.py']
         #cmd = [r'C:\local\winpty\bin\console.exe', r'C:\local\mingw64\msys\1.0\bin\bash.exe']
-        cmd = [r'C:\Users\stone\GitHub\winpty\build\winpty.exe', r'C:\local\mingw64\msys\1.0\bin\bash.exe' , '--login']
+        cmd = [r'C:\Users\stone\GitHub\winpty\build\winpty.exe',
+               r'--pipe',
+               r'C:\local\mingw64\msys\1.0\bin\bash.exe' , '--login']
         p = Popen(cmd,
                   stdin=PIPE, stdout=PIPE, stderr=STDOUT, shell=True)
         session.interactive_shell(p)
