5a14b71b6b57a2dc6d32badf353c3b27bb8143aa
stonewell <jingnan.si@gmail.com>
2017-03-20 23:01:15 -0700

    clean up unused code in cap_manager

--- pymterm/cap/cap_manager.py
+++ pymterm/cap/cap_manager.py
@@ -6,16 +6,12 @@ import unknown_cap
 
 def get_cap_handler(name):
     logging.getLogger('cap_manager').debug("cap:{}".format(name))
-    
+
     # Fast path: see if the module has already been imported.
     try:
-        return sys.modules[name]
+        return sys.modules['cap.' + name]
     except KeyError:
         pass
-
-    # If any of the following calls raises an exception,
-    # there's a problem we can't handle -- let the caller handle it.
-    fp = None
 
     try:
         #fp, pathname, description = imp.find_module(name, [os.path.dirname(__file__)])
@@ -25,7 +21,3 @@ def get_cap_handler(name):
         return sys.modules['cap.' + name]
     except ImportError:
         return unknown_cap
-    finally:
-        # Since we may exit via an exception, close fp explicitly.
-        if fp:
-            fp.close()
